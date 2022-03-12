ca013c5554783304c045eba0357046458f3fbfb4
朱正清 <zhengqing.zhu@tcl.com>
2022-03-12 22:17:47 +0800

    1

--- Chapter15-Explain执行计划/01_Explain执行计划.sql
+++ Chapter15-Explain执行计划/01_Explain执行计划.sql
@@ -105,7 +105,7 @@ where id = (select id from employee where id = (select id from customer where id
 EXPLAIN
 select *
 from department d,
-     (select * from employee group by dep_id) t
+     (select dep_id from employee group by dep_id) t
 where d.id = t.dep_id;
 
 -- select_type: UNION, UNION RESULT
