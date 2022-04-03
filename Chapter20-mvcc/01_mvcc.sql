1a5d38106ab8571416150e6ee04e398206af3616
朱正清 <zhengqing.zhu@tcl.com>
2022-04-03 21:56:14 +0800

    1

---
+++ Chapter20-mvcc/01_mvcc.sql
@@ -0,0 +1,8 @@
+/*
+ * 资料：
+ * 1. https://www.bilibili.com/video/BV1t5411u7Fg?p=1
+ * 2. https://www.bilibili.com/video/BV1hL411479T/?spm_id_from=333.788.recommend_more_video.6
+ * 3. https://www.bilibili.com/video/BV1YJ411J7vb/?spm_id_from=333.788.recommend_more_video.-1
+ * 4. https://zhuanlan.zhihu.com/p/421769708
+ * 5. https://zhuanlan.zhihu.com/p/52977862
+ */
