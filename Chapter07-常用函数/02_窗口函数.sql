81344a0a40299e3503275e4d21b4e21d726f5ac5
朱正清 <zhengqing.zhu@tcl.com>
2022-05-31 05:45:37 +0800

    1

---
+++ Chapter07-常用函数/02_窗口函数.sql
@@ -0,0 +1,34 @@
+/*
+ * ref:
+ * 1. https://zhuanlan.zhihu.com/p/409561328
+ * 2. https://blog.csdn.net/luobing_csdn/article/details/103156538
+ */
+
+CREATE TABLE sales
+(
+    employee VARCHAR(50)    NOT NULL,
+    year     INT            NOT NULL,
+    month    INT            NOT NULL,
+    sale     DECIMAL(14, 2) NOT NULL,
+    PRIMARY KEY (employee, year, month)
+);
+
+INSERT INTO sales(employee, year, month, sale)
+VALUES ('Alice', 2016, 01, 100),
+       ('Alice', 2016, 02, 100),
+       ('Alice', 2016, 03, 100),
+       ('Alice', 2017, 01, 130),
+       ('Alice', 2017, 02, 130),
+       ('Alice', 2017, 03, 130),
+       ('Bob', 2016, 01, 110),
+       ('Bob', 2016, 02, 110),
+       ('Bob', 2016, 03, 110),
+       ('Bob', 2017, 01, 120),
+       ('Bob', 2017, 02, 120),
+       ('Bob', 2017, 03, 120),
+       ('John', 2016, 01, 200),
+       ('John', 2016, 02, 200),
+       ('John', 2016, 03, 200),
+       ('John', 2017, 01, 150),
+       ('John', 2017, 02, 150),
+       ('John', 2017, 03, 150);
--- 面试题/01_基于扫码记录查找密接人员/题解.sql
+++ 面试题/01_基于扫码记录查找密接人员/题解.sql
@@ -1,3 +1,4 @@
 -- ref: https://www.bilibili.com/video/BV1g44y137Az?spm_id_from=333.1007.top_right_bar_window_history.content.click
 
-
+-- 问题一：如何找出用户在每个区域的停留开始时间和结束时间
+-- 窗口函数简介：https://zhuanlan.zhihu.com/p/409561328
